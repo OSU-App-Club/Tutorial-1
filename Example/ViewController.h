2b1573db43381fa6869b759cef2699dcf6392877
cvanderschuere <cdvconcepts@gmail.com>
2012-10-19 19:40:37 -0700

    Initial Commit (Meeting 1 10/19)

--- Example/ViewController.h
+++ Example/ViewController.h
@@ -10,4 +10,18 @@
 
 @interface ViewController : UIViewController
 
+//Add UIButton property (This is weak because we create it in Inteface Builder)
+@property (nonatomic, weak) IBOutlet UIButton *exampleButton;
+//These properties need to be strong so they are never released. free()
+@property (nonatomic, strong) UILabel *example;
+@property (nonatomic, strong) UIStepper *stepper;
+@property int count;
+
+//Use IBAction to make methods appear in Interface Builder (returns void)
+-(IBAction)incrementCounterLabel:(id)sender;
+
+//We connected this method to the UIStepper in code in viewDidLoad
+//The UIStepper that sends the action will be included with the "sender" paramater
+-(void) stepperSelectorAction:(UIStepper *) sender;
+ 
 @end
--- Example/ViewController.m
+++ Example/ViewController.m
@@ -14,16 +14,48 @@
 
 @implementation ViewController
 
+@synthesize count, exampleButton, example;
+@synthesize stepper;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
+    
+    /*
+        This a method, viewDidLoad, that gets called once on startup. You can put all things you need to setup up before the view becomes visible in this method
+    */
+    
+    //Create a Label
+    self.example = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 400, 100)];
+    
+    //Set property of UILabel example with an nsstring
+    self.example.text = @"Hello World!";
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //Add to view. You need to add things as subviews of the view before they will become visible on the screen.
+    [self.view addSubview:self.example];
+    
+    //Modify View. You can change all sorts of properties of the view like this...
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    //Create stepper (Alloc/init, add method to call on action, add to view) 
+    self.stepper = [[UIStepper alloc] initWithFrame:CGRectMake(0, 200, 50,20)];
+    [self.stepper addTarget:self action:@selector(stepperSelectorAction:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.stepper];
+    
+}
+//This method gets called whenever you "touch up inside" on our uibutton (We did this with Interface Builder, MainStoryboard.storyboard)
+-(IBAction)incrementCounterLabel:(id)sender{
+    
+    self.count++; // count = count + 1
+    
+    //Set the "text" property of our label to a string
+    self.example.text = [NSString stringWithFormat:@"Count: %d", self.count]; //stringWithFormat works very similar printf in C
+}
+-(void) stepperSelectorAction:(UIStepper *) sender{
+    //This gets called on UIControlEventValueChanged like we setup in viewDidLoad
+    self.example.text = [NSString stringWithFormat:@"Count: %f", sender.value];
+    
 }
 
 @end
